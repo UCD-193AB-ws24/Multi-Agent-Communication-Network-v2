99b4be1ffde143d8eb34f80b384039377d55cfe0
Honghui-Li-8 <honghuili8@gmail.com>
2024-05-14 14:39:35 -0700

    python uart encode, decode confirmed: - fixed send to add \ff and \fe on message - add a uart_test file to tet edge node

--- Arduino/Send_cmd/Send_cmd.ino
+++ Arduino/Send_cmd/Send_cmd.ino
@@ -7,7 +7,8 @@
 #define UART_START 0xFF
 #define UART_END 0xFE
 
-void uart_write_encoded_bytes(byte* ble_cmd, size_t ble_cmd_len, byte* data_buffer, size_t data_length) {
+// log to Serial port that's connected with usb
+void uart_log_encoded_bytes(byte* ble_cmd, size_t ble_cmd_len, byte* data_buffer, size_t data_length) {
   byte esacpe_byte = ESCAPE_BYTE;
   byte uart_start = UART_START;
   byte uart_end = UART_END;
@@ -41,6 +42,44 @@ void uart_write_encoded_bytes(byte* ble_cmd, size_t ble_cmd_len, byte* data_buff
   }
 
   Serial.write(&uart_end, 1);
+  Serial.println();
+}
+
+// write to Serial1 port that's connected tx/rx pin
+void uart_write_encoded_bytes(byte* ble_cmd, size_t ble_cmd_len, byte* data_buffer, size_t data_length) {
+  byte esacpe_byte = ESCAPE_BYTE;
+  byte uart_start = UART_START;
+  byte uart_end = UART_END;
+  
+  Serial1.write(&uart_start, 1);
+
+  // encode ble_cmd
+  for (byte* byte_itr = ble_cmd ; byte_itr < ble_cmd + ble_cmd_len; ++byte_itr) {
+    if (byte_itr[0] < esacpe_byte) {
+      Serial1.write(byte_itr, 1);
+      continue;
+    }
+
+    // need 2 byte encoded
+    byte encoded = byte_itr[0] ^ esacpe_byte; // bitwise Xor
+    Serial1.write(&esacpe_byte, 1);
+    Serial1.write(&encoded, 1);
+  }
+  
+  // encode data_buffer
+  for (byte* byte_itr = data_buffer ; byte_itr < data_buffer + data_length; ++byte_itr) {
+    if (byte_itr[0] < esacpe_byte) {
+      Serial1.write(byte_itr, 1);
+      continue;
+    }
+
+    // need 2 byte encoded
+    byte encoded = byte_itr[0] ^ esacpe_byte; // bitwise Xor
+    Serial1.write(&esacpe_byte, 1);
+    Serial1.write(&encoded, 1);
+  }
+
+  Serial1.write(&uart_end, 1);
 }
 
 void ble_send_to_root(byte* data_buffer, size_t data_length) {
@@ -55,6 +94,7 @@ void ble_send_to_root(byte* data_buffer, size_t data_length) {
   // write length
   ble_cmd[7] = (byte) data_length;
   uart_write_encoded_bytes(ble_cmd, 8, data_buffer, data_length);
+  // uart_log_encoded_bytes(ble_cmd, 8, data_buffer, data_length);
 }
 
 void sendGPS(int p1, int p2, int p3) {
@@ -172,6 +212,51 @@ void sendTestMutipleData(int16_t *fake_gps, int16_t *fake_ldc, int8_t *fake_idx)
   ble_send_to_root(buffer, buf_itr - buffer);
 }
 
+// Read and Decode uart message
+size_t uart_readAndDecode_message(class Uart serial_port, byte* data_buffer, size_t buffer_len, size_t* data_len_ptr) {
+  size_t byte_read = 0;
+  size_t data_len = 0;
+  
+  // locate uart start
+  while (serial_port.available() > 0) {
+    byte data = serial_port.read();
+    if (data == UART_START) {
+      break;
+    }
+  }
+
+  // read untile uart end
+  while (serial_port.available() > 0) {
+    byte data = serial_port.read();
+    if (data == UART_END) {
+      break;
+    }
+
+    if (data == ESCAPE_BYTE) {
+      byte encoded = serial_port.read();
+      data = encoded ^ ESCAPE_BYTE;
+      byte_read += 1;
+    }
+
+    // store this byte to data_buffer
+    data_buffer[data_len] = data;
+    byte_read += 1;
+    data_len += 1;
+  }
+
+  if (data_len > buffer_len) {
+    Serial.printf("[Error] Read %d byte decoed to %d byte > %d byte buffer length !!", byte_read, data_len, buffer_len);
+  }
+
+  *data_len_ptr = data_len;
+
+
+  // Debug log
+  Serial.print("[UART] >> \'");
+  Serial.write(data_buffer, byte_read);
+  Serial.println("\'");
+}
+
 void setup() {
   Serial.begin(115200); // usb monitor
   // while (!Serial);
@@ -182,11 +267,15 @@ void setup() {
 int16_t fake_gps = 64000;
 int16_t fake_lds = 59910;
 int8_t fake_idx = 0;
+
 void loop() {
   sendTestMutipleData(&fake_gps, &fake_lds, &fake_idx);
   fake_gps += 20;
   fake_lds += 10;
   fake_idx += 1;
-
+  byte data[1024];
+  size_t data_len;
+  uart_readAndDecode_message(Serial1, data, 1024, &data_len);
+  uart_readAndDecode_message(Serial1, data, 1024, &data_len);
   delay(5000);
 }
